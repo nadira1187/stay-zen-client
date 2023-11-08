a1967d4a00fa50beb44a53ce7c69164d859c1a31
nadira1187 <nadira35-1187@s.diu.edu.bd>
2023-11-08 08:18:44 +0600

    added update the availability of room in client side

--- src/pages/bookroom/BookRoom.jsx
+++ src/pages/bookroom/BookRoom.jsx
@@ -8,11 +8,11 @@ import { Helmet } from "react-helmet";
 
 const BookRoom = () => {
 
-    const room =useLoaderData([]);
-    const {description,pricePerNight,images,_id,availability}=room;
-    const {user}=useContext(AuthContext);
-    const handleBookService = event =>{
-      
+    const room = useLoaderData([]);
+    const { description, pricePerNight, images, _id, availability } = room;
+    const { user } = useContext(AuthContext);
+    const handleBookService = event => {
+
         event.preventDefault();
 
         const form = event.target;
@@ -20,79 +20,97 @@ const BookRoom = () => {
         const date = form.date.value;
         const email = user?.email;
         const booking = {
-            customerName: name, 
-            email, 
+            customerName: name,
+            email,
             images,
-            date, 
+            date,
             availability,
             service: description,
-            service_id: _id, 
+            service_id: _id,
             price: pricePerNight,
         }
 
         console.log(booking);
 
         fetch('http://localhost:5000/bookings', {
-            method: 'POST', 
+            method: 'POST',
             headers: {
                 'content-type': 'application/json'
-            }, 
+            },
             body: JSON.stringify(booking)
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.insertedId){
-                swal('room booked successfully')
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    // Successfully created the booking, now update availability
+                    const newAvailability = availability - 1;
+
+                    // Send a PATCH request to update room availability
+                    fetch(`http://localhost:5000/roomss/${_id}`, {
+                        method: "PATCH",
+                        headers: {
+                            "content-type": "application/json",
+                        },
+                        body: JSON.stringify({ availability: newAvailability }),
+                    })
+                        .then((res) => res.json())
+                        .then((updatedData) => {
+                            console.log(updatedData);
+                            if (updatedData.modifiedCount > 0) {
+                                // Room availability updated successfully
+                                swal("Room booked successfully");
+                            }
+                        });
+                }
+            })
 
     }
     return (
         <div>
-             <Helmet>
+            <Helmet>
                 <title>Book Room</title>
             </Helmet>
             <Navbar></Navbar>
-              <div className="p-4">
-        <h2 className='text-center text-3xl font-mono font-bold text-red-900'>Book Service: {description} </h2>
-        <form onSubmit={handleBookService}>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Name</span>
-                    </label>
-                    <input type="text" defaultValue={user?.displayName} name="name" className="input input-accent input-bordered" />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Date</span>
-                    </label>
-                    <input type="date" name="date" className="input input-accent input-bordered" />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input type="text" name="email" defaultValue={user?.email} placeholder="email" className="input input-accent input-bordered" />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Due amount</span>
-                    </label>
-                    <input type="text" defaultValue={'$'+ pricePerNight} className="input input-accent input-bordered" />
-                </div>
-            </div>
-            <div className="form-control mt-6">
-                <input className="btn btn-primary bg-red-900 border-none text-white normal-case btn-block" type="submit" value="Book Room" />
-            </div>
-        </form>
-        <div className="card-body">
+            <div className="p-4">
+                <h2 className='text-center text-3xl font-mono font-bold text-red-900'>Book Service: {description} </h2>
+                <form onSubmit={handleBookService}>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" defaultValue={user?.displayName} name="name" className="input input-accent input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Date</span>
+                            </label>
+                            <input type="date" name="date" className="input input-accent input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name="email" defaultValue={user?.email} placeholder="email" className="input input-accent input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Due amount</span>
+                            </label>
+                            <input type="text" defaultValue={'$' + pricePerNight} className="input input-accent input-bordered" />
+                        </div>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input className="btn btn-primary bg-red-900 border-none text-white normal-case btn-block" type="submit" value="Book Room" />
+                    </div>
+                </form>
+                <div className="card-body">
 
+                </div>
+            </div>
         </div>
-    </div>
-        </div>
-      
+
     );
 };
 
